4ab5dc2625286e6040c52b3b267c689c55e2db36
Joshua Vigel <joshv@wireless-10-148-146-114.public.utexas.edu>
2026-02-21 23:00:37 -0600

    erm2

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import Surprise from './Surprise';
 
 function App() {
 	
-<<<<<<< HEAD
 	const [stage, setStage] = useState(4);
-=======
-	const [stage, setStage] = useState(4);
->>>>>>> 2d2275155d2a5ad8bab43182e8d1bc4f6050289f
 			
   return (
     <div className="App">
